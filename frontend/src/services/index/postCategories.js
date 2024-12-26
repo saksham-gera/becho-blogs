f6b313d73a80713dd563b227e5251399074fe068
Saksham Gera <sakshamgera1456@gmail.com>
2024-12-27 01:37:01 +0530

    Blog Website Completed

---
+++ frontend/src/services/index/postCategories.js
@@ -0,0 +1,88 @@
+import axios from "axios";
+
+export const getAllCategories = async (
+  searchKeyword = "",
+  page = 1,
+  limit = 10
+) => {
+  try {
+    const { data, headers } = await axios.get(
+      `http://localhost:6969/api/post-categories?searchKeyword=${searchKeyword}&page=${page}&limit=${limit}`
+    );
+    return { data, headers };
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
+
+export const deleteCategory = async ({ slug, token }) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const { data } = await axios.delete(`http://localhost:6969/api/post-categories/${slug}`, config);
+    return data;
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
+
+export const createCategory = async ({ token, title }) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const { data } = await axios.post(
+      `http://localhost:6969/api/post-categories`,
+      { title },
+      config
+    );
+    return data;
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
+
+export const updateCategory = async ({ token, title, slug }) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const { data } = await axios.put(
+      `http://localhost:6969/api/post-categories/${slug}`,
+      { title },
+      config
+    );
+    return data;
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
+
+export const getSingleCategory = async ({ slug }) => {
+  try {
+    const { data } = await axios.get(`http://localhost:6969/api/post-categories/${slug}`);
+    return data;
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
